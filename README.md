cc09352612593a3639374b6871c2004b30751c31
Tao Peng <ptpttt@gmail.com>
2013-03-22 11:02:32 +0100

    Rewrite in CoffeeScript; Add example.

--- README.md
+++ README.md
@@ -1,4 +1,55 @@
-A [sliding puzzle](http://en.wikipedia.org/wiki/Sliding_puzzle) game
-implementation with Javascript and JQuery.
+## Sliding Puzzle
 
-This is part of our [final project](http://gavleslidingpuzzle.appspot.com/) of the Software Development course in 2011.
+A [sliding puzzle](http://en.wikipedia.org/wiki/Sliding_puzzle) game
+written in CoffeeScript. It uses jQuery and provides its functionality
+as a jQuery plugin.
+
+## How to use
+
+1. Copy `puzzle.js` to your site folder.
+
+2. Add a `div` element to your HTML file. For example:
+
+````html
+    <div id="puzzle">puzzle shows here</div>
+````
+
+3. Include jQuery and `puzzle.js` in your HTML file.
+
+````html
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script type="text/javascript" src="puzzle.js"></script>
+````
+
+4. Choose a nice picture as the background image, then initialize the
+   game.
+
+````javascript
+    var game = $('#puzzle').puzzle({image: 'url(PATH/TO/YOUR/IMG)',
+                                    spacing: 4,
+                                    rows: 4,
+                                    cols: 4});
+````
+
+5. Let the game know how to move, and how to react when you win.
+
+````javascript
+    game.bind('click', function () {
+        this.steps();
+    }).bind('done', function () {
+        alert('Well done!');
+    }).shuffle();
+````
+
+## History
+
+This was originally part of our
+[final project](http://gavleslidingpuzzle.appspot.com/) of the
+Software Development course in 2011.
+
+In March 2013, it was rewritten in CoffeeScript.
+
+## License
+
+puzzle.js is under the MIT License. See LICENSE file for full license
+text.
---
+++ js/main.js
@@ -0,0 +1,108 @@
+$(function () {
+    var MAX_ROW = 20, MAX_COL = 20;
+
+    var game = $('#puzzle').puzzle({image: 'url(img/gavle.jpg)',
+                                    spacing: 4,
+                                    rows: 4,
+                                    cols: 4});
+
+    click = function () {
+        this.steps();
+    }
+
+    timer = {
+        startTime: null,
+
+        endTime: null,
+
+        start: function () {
+            this.startTime = new Date();
+            return this.startTime;
+        },
+
+        end: function () {
+            this.endTime = new Date();
+            return this.endTime;
+        },
+
+        reset: function () {
+            this.startTime = null;
+            this.endTime = null;
+        },
+
+        totalSeconds: function () {
+            endTime = this.endTime || new Date();
+            if (this.startTime) {
+                return Math.floor((endTime - this.startTime)/1000);
+            } else {
+                return 0;
+            }
+        }
+    };
+
+    done = function () {
+        timer.end();
+        alert('Total time: ' + timer.totalSeconds() + ' seconds\n'
+              + 'Steps: ' + $('#steps').text());
+        game.unbind('click', click);
+        game.div.css({'background-color': '#222',
+                      'border-color': '#222'});
+    }
+
+    game.bind('puzzle.shuffle', function () {
+        timer.start();
+        game.unbind('click', click).bind('click', click);
+        game.unbind('puzzle.done', done).one('puzzle.done', done);
+        game.div.css({'background-color': 'black',
+                      'border-color': 'black'});
+        $('#steps').text(0);
+
+    }).bind('puzzle.step', function () {
+        $('#steps').text(parseInt($('#steps').text()) + 1);
+
+    }).shuffle();
+
+    $('#shuffle').click(function () {
+        game.shuffle();
+    });
+
+    $('#background').click(function () {
+        if (game.image === 'url(img/gavle.jpg)') {
+            $('#puzzle').width(720).height(480);
+            game.set({image: 'url(img/lake.jpg)', spacing: 2});
+        } else {
+            $('#puzzle').width(800).height(600);
+            game.set({image: 'url(img/gavle.jpg)', spacing: 4});
+        }
+
+
+    });
+
+    $('#rows-inc').click(function () {
+        if (game.rows < MAX_ROW) {
+            game.set({rows: game.rows + 1});
+            game.shuffle();
+        }
+    });
+
+    $('#rows-dec').click(function () {
+        if (game.rows > 2) {
+            game.set({rows: game.rows - 1});
+            game.shuffle();
+        }
+    });
+
+    $('#cols-inc').click(function () {
+        if (game.cols < MAX_COL) {
+            game.set({cols: game.cols + 1});
+            game.shuffle();
+        }
+    });
+
+    $('#cols-dec').click(function () {
+        if (game.cols > 2) {
+            game.set({cols: game.cols - 1});
+            game.shuffle();
+        }
+    });
+});
